3ecb36f6406a4b75b819eafccac61ac0ed7908ca
vitor.avelino <vitor.avelino@concretesolutions.com.br>
2017-12-09 19:25:43 -0200

    Domínio

---
+++ Pubs.Dominio/EntidadeBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Pubs.Dominio
+{
+    public abstract class EntidadeBase
+    {
+        protected EntidadeBase()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
