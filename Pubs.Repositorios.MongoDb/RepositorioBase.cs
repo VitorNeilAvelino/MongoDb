389b6d7cdc2e5896faa59f258ba734b8ba10ff5a
vitor.avelino <vitor.avelino@concretesolutions.com.br>
2017-12-10 13:01:30 -0200

    Conexão com o MongoDb

--- Pubs.Repositorios.MongoDb/RepositorioBase.cs
+++ Pubs.Repositorios.MongoDb/RepositorioBase.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,16 @@ namespace Pubs.Repositorios.MongoDb
 {
     public class RepositorioBase<T>
     {
+        private IMongoCollection<T> _colecao;
+        private IMongoDatabase _db;
 
+        public RepositorioBase()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["pubsConnectionString"].ConnectionString;
+            var databaseName = MongoUrl.Create(connectionString).DatabaseName;
+
+            _db = new MongoClient(connectionString).GetDatabase(databaseName);
+            _colecao = _db.GetCollection<T>(typeof(T).Name);
+        }
     }
 }
